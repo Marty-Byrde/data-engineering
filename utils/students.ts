7e3359e2f03d917e00b55602b989ea9ad8b8c289
Marty Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2024-04-30 13:22:09 +0200

    feat: created student-validation module
    This validation-function checks whether a student with a given matricelnumber (matno) exists in the database. In case he does not he will be created.

---
+++ utils/students.ts
@@ -0,0 +1,20 @@
+import { importConfig } from '../index'
+import { logQueryResult } from '../log/LogQueryResult'
+
+/**
+ * Checks whether a student that is identified by his matricelnumber (matno) exists
+ * @param matno The property that realtes to the studentkey
+ * @param name The name that is used to insert the student in case he does not exist
+ * @param client The database-client
+ */
+export async function validateStudent(matno: string, name: string, client: importConfig['client']){
+  const result = await client.query(`Select name from student where studentkey = '${matno}'`)
+
+  if(!!result.rowCount) return true;
+
+  // Otherwise create insert student
+  const args = [matno, name]
+  const query = `INSERT INTO student (studentkey, name) VALUES (${args.map(el => `'${el}'`).join(', ')})`
+  const queryResult = await client.query(query)
+  logQueryResult(queryResult, "student");
+}
